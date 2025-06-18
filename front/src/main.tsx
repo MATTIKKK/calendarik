c8f9e1060007707c2e62558c1fd8d356887f885d
Your Name <you@example.com>
2025-06-18 16:55:56 +0500

    HashRouter wa changed to BrowserRouter

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext.tsx';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-  <HashRouter>
+  <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </HashRouter>
+  </BrowserRouter>
   // </StrictMode>
 );
