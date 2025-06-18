434983e3bff171f797017915fadc5cfb3387e051
Your Name <you@example.com>
2025-06-18 16:03:10 +0500

    hashrouter is added

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext.tsx';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+  <HashRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </HashRouter>
   // </StrictMode>
 );
